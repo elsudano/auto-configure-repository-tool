4a74a6ffc2549ef3002d270a513f83171b99deb9
Carlos <carlos.ariel.de.la.torre@gmail.com>
2022-06-11 04:51:36 +0200

    Update acrt.go

--- acrt/acrt.go
+++ acrt/acrt.go
@@ -36,6 +36,60 @@ func (c *Client) ShowLogin() string {
 	return query.Viewer.Login
 }
 
+func (c *Client) Last10Repositories(owner string) {
+	// type myRepository struct {
+	// 	Name   githubv4.String
+	// 	sshURL githubv4.String
+	// }
+
+	var query struct {
+		User struct {
+			Id           githubv4.ID
+			Repositories struct {
+				// totalCount githubv4.Int
+				// Nodes    []myRepository
+				// PageInfo struct {
+				// 	EndCursor   githubv4.String
+				// 	HasNextPage bool
+				// }
+			} `graphql:"repositories(first: 10, orderBy: {field: CREATED_AT, direction: DESC}, after: $RepositoriesCursor)"`
+		} `graphql:"user(login: $owner)"`
+		RateLimit struct {
+			Limit     githubv4.Int
+			Cost      githubv4.Int
+			Remaining githubv4.Int
+			ResetAt   githubv4.DateTime
+		}
+	}
+	variables := map[string]interface{}{
+		"owner": githubv4.String(owner),
+		// "RepositoriesCursor": (*githubv4.String)(nil),
+	}
+	// var allRepositories []myRepository
+	// for {
+	err := c.Client.Query(c.Context, &query, variables)
+	if err != nil {
+		log.Printf("[ACRT] Fi: acrt.go Fu: Last10Repositories Obj:Error %s\n", err)
+	} else {
+		fmt.Println("    Id:", query.User.Id)
+		// fmt.Println(" Count:", query.User.Repositories.totalCount)
+		// fmt.Println(" Repo name:", query.User.Repositories.nodes.name)
+		// fmt.Println("    sshURL:", query.User.Repositories.nodes.sshUrl)
+		if c.debugIsEnabled() {
+			fmt.Println(" Rate Limit:", query.RateLimit.Limit)
+			fmt.Println("       Cost:", query.RateLimit.Cost)
+			fmt.Println("  Remaining:", query.RateLimit.Remaining)
+			fmt.Println("   Reset At:", query.RateLimit.ResetAt)
+		}
+	}
+	// 	allRepositories = append(allRepositories, query.User.Repositories.Nodes...)
+	// 	if !query.User.Repositories.PageInfo.HasNextPage {
+	// 		break
+	// 	}
+	//   variables["RepositoriesCursor"] = githubv4.NewString(query.User.Repositories.PageInfo.EndCursor)
+	// }
+}
+
 func (c *Client) ShowRepo(owner string, repo string) {
 	var query struct {
 		Repository struct {
