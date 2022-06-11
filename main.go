1a0c4b8ce7ca1eee8aafd7df1b0c0e9fa0641e1e
Carlos <carlos.ariel.de.la.torre@gmail.com>
2022-06-11 04:52:32 +0200

    Update main.go

--- main.go
+++ main.go
@@ -8,19 +8,20 @@ import (
 )
 
 func main() {
-	token := flag.String("token", "", "you need put your access token in order to make actions in your repository")
 	repo := flag.String("repo", "", "You need put the URL to the repo that you want to read")
 	flag.Parse()
 
 	if *repo != "" {
-		fmt.Printf(*token + "\n")
 		fmt.Printf(*repo + "\n")
 		client, err := acrt.NewClient()
 		if err != nil {
 			log.Printf("[ACRT] Fi: main.go Fu: main Obj:GitHub Client %#v\n", client.Client)
 		}
 		client.Debug = true
-		client.ShowRepo(client.ShowLogin(), *repo)
+		login := client.ShowLogin()
+		fmt.Printf("Login: %v\n", login)
+		client.Last10Repositories(login)
+		client.ShowRepo(login, *repo)
 	} else {
 		flag.Usage()
 	}
